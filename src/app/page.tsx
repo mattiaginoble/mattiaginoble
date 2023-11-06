616300885e48c296ee1cd4d6a26c2aac5aaca19b
mattiaginoble <mattia.ginoble@gmail.com>
2023-11-06 10:59:22 +0100

    feat: Add Homepage Page Type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
-import { createClient } from "@/prismicio";
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className="text-5x1 font-display">Hello World!</h1>
-    </main>
-  )
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <div className="text-red-500">Homepage!</div>;
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
