e8f57801ffe326ce268438d7e51c84b720c02a23
mattiaginoble <mattia.ginoble@gmail.com>
2023-12-22 15:35:05 +0100

    fix: header pb-0 and z-index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
   return (
     <Bounded
       as="header"
-      className="py-4 md:py-6 lg:py-8 pb-0 sticky top-0 bg-white dark:bg-[#141414] dark:text-white"
+      className="py-4 md:py-6 lg:py-8 pb-0 md:pb-0 lg:pb-0 sticky top-0 bg-white dark:bg-[#141414] dark:text-white z-10"
     >
       <div className="flex gap-4 items-center justify-between flex-row border-b dark:border-[#454545] pb-4 sm:text-2xl text-lg font-semibold font-display">
         <Link href="/">{settings.data.site_title}</Link>
